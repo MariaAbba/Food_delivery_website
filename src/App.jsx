ac4e8d083ca605f2997d030deebf3bdcfa557882
Mary <gretchen7887@gmail.com>
2025-03-20 11:21:55 +0400

    Replaced old project with a new one

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useEffect } from 'react'
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Cart from './pages/Cart/Cart'
+import Home from './pages/Home/Home'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
-import './App.css'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Footer from './components/Footer'
-import Aos from 'aos'
-import 'aos/dist/aos.css'
-import Popular from './components/Popular'
-import Offers from './components/Offers'
-import Tour from './components/Tour'
-import Explore from './components/Explore'
-import Blog from './components/Blog'
-
-function App() {
-  useEffect(() => {
-    Aos.init({
-      duration: 1500,
-    })
-  }, [])
-
+const App = () => {
   return (
-    <>
-      <Header />
-      <Hero />
-      <Popular />
-      <Offers />
-      <Tour />
-      <Explore />
-      <Blog />
-      <Footer />
-    </>
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
+      </Routes>
+    </div>
   )
 }
 
